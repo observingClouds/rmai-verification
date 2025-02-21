37d099ff7b29f020d680ff592b8424703b1398c6
Michiel Van Ginderachter <michiel.vanginderachter@meteo.be>
2025-02-21 10:46:03 +0200

    refactor metrics

---
+++ metrics/__init__.py
@@ -0,0 +1,32 @@
+import xarray as xr
+import logging
+
+from . import xskill
+from typing import List, Dict
+from utils.sanitation import concat_dict_along_keys
+
+METRICS = {
+    "xskillscore" : {
+        "rmse": xskill.rmse,
+        "mse": xskill.mse,
+        "bias": xskill.bias,
+    },
+}
+
+LOG = logging.getLogger(__name__)
+
+def calculate_metrics(reference : xr.Dataset, dict_of_datasets: Dict[str, xr.Dataset], package : str, metrics : List[str], avg_dims : str | List[str] ):
+    LOG.debug(dict_of_datasets.keys())
+    metrics_dict = dict()
+    for metric_name in metrics:
+        _metric = dict()
+        metric = METRICS[package][metric_name]
+        for name, model in dict_of_datasets.items():
+            LOG.info(f"Calulating {metric_name} for model {name}")
+            _reference = reference.sel(
+                valid_time=model["valid_time"]
+            )
+            reference, model = xr.align(reference,model)
+            _metric[name]=metric(_reference,model,avg_dims)
+        metrics_dict[metric_name] = concat_dict_along_keys(_metric, "model")
+    return concat_dict_along_keys(metrics_dict, "metric")
