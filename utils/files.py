9f5c32d3ec1a1642df93c9dee78c29caae40cb57
Michiel Van Ginderachter <michiel.vanginderachter@meteo.be>
2025-02-21 12:21:03 +0200

    add util functionality

--- utils/files.py
+++ utils/files.py
@@ -1,4 +1,6 @@
 from datetime  import datetime
+import yaml
+import argparse
 import os 
 import logging
 
@@ -28,3 +30,28 @@ def get_filenames(path_fmt,start,end,frequency):
     if len(filenames) == 1: 
         filenames = filenames[0]
     return filenames
+
+def write_yaml(x,fn,sort=False):
+    with open (fn,'w') as f:
+        yaml.dump(x,f,sort_keys=False)
+
+def load_yaml(fn):
+    with open(fn,'r') as f:
+        return yaml.safe_load(f)
+    
+
+def yaml_file_type(filename):
+    """Custom argparse type for YAML file validation."""
+    if not os.path.isfile(filename):
+        raise argparse.ArgumentTypeError(f"File '{filename}' does not exist.")
+    if not (filename.endswith(".yaml") or filename.endswith(".yml")):
+        raise argparse.ArgumentTypeError("Config file must have a .yaml or .yml extension.")
+    
+    # Try to load YAML file to check for syntax errors
+    try:
+        with open(filename, "r") as f:
+            yaml.safe_load(f)
+    except yaml.YAMLError as e:
+        raise argparse.ArgumentTypeError(f"Invalid YAML file: {e}")
+
+    return filename  # Return the valid filename
