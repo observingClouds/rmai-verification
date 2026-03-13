c04d400343a00ca5e2c2632ee6fdc1cf89bca77d
Hauke Schulz <43613877+observingClouds@users.noreply.github.com>
2026-03-13 13:32:40 +0100

    add CARRA projection mappings

--- src/rmai_verification/grids/__init__.py
+++ src/rmai_verification/grids/__init__.py
@@ -22,6 +22,46 @@ GRIDS = dict(
       ny=1069
     ),
   ),
+  carra_west=dict(
+    projection="lcc",
+    projection_kws=dict(
+        globe=dict(
+            semimajor_axis=6371229.0,
+            semiminor_axis=6371229.0,
+        ),
+        central_longitude=324.0,
+        central_latitude=72.0,
+        standard_parallels=[72.0, 72.0],
+    ),
+    grid_kws=dict(
+        lower_left=(-57.09699999999995, 55.80999999999999),
+        upper_right=(37.63491110786931, 77.83194769476704),
+        delta_x=2500.0,
+        delta_y=2500.0,
+        nx=1069,
+        ny=1269
+    ),
+  ),
+  carra_east=dict(
+    projection="lcc",
+    projection_kws=dict(
+        globe=dict(
+            semimajor_axis=6371229.0,
+            semiminor_axis=6371229.0,
+        ),
+        central_longitude=326.0,
+        central_latitude=80.0,
+        standard_parallels=[80.0, 80.0],
+    ),
+    grid_kws=dict(
+        lower_left=(-19.407999999999994, 70.13499999999999),
+        upper_right=(64.60843181916762, 67.33009041873429),
+        delta_x=2500.0,
+        delta_y=2500.0,
+        nx=789,
+        ny=989
+    ),
+  ),
 )
 
 PROJECTIONS=dict(
