3a0790af6059c49ada787e6af224e61cd8975f29
Hauke Schulz <43613877+observingClouds@users.noreply.github.com>
2026-04-22 10:56:04 +0200

    support some other anemoi inference format

--- src/rmai_verification/datastores/anemoi_inference.py
+++ src/rmai_verification/datastores/anemoi_inference.py
@@ -36,12 +36,21 @@ MF_KWARGS = {
 
 class AnemoiInference(GridDataStore,FcstDataStore):
     def __init__(self, 
-                 filename_or_obj: Union[str, List[str], xr.Dataset, List[xr.Dataset]], 
+                 files: Union[str, List[str], xr.Dataset, List[xr.Dataset], None] = None,
+                 filename_or_obj: Union[str, List[str], xr.Dataset, List[xr.Dataset], None] = None,
                  variables: Union[List[str],Tuple[str],set] = None,
                  mapping: Union[Dict[str,str],str] = None,
                  mf_kwargs: Dict[str,str] = dict()
                  ):
         LOG.info("Initializing AnemoiInference datastore")
+
+        if filename_or_obj is not None and files is not None:
+            raise ValueError("Provide only one of `files` or `filename_or_obj`")
+        if filename_or_obj is None:
+            filename_or_obj = files
+        if filename_or_obj is None:
+            raise ValueError("Missing input data, please provide `files` or `filename_or_obj`")
+
         self._filename_or_obj = filename_or_obj
         self._mapping: Union[Dict[str],str] = mapping
         self._stacked: bool = True
@@ -71,6 +80,10 @@ class AnemoiInference(GridDataStore,FcstDataStore):
             ds = xr.open_dataset(self._filename_or_obj, engine=self._mf_kwargs["engine"])
             close_ds = True
 
+        self._input_format = _infer_input_format(ds)
+        if self._input_format == "structured":
+            self._stacked = False
+
         # Get the longitudes and latitude 
         self._longitudes = ds["longitude"].data
         self._latitudes = ds["latitude"].data
@@ -86,8 +99,10 @@ class AnemoiInference(GridDataStore,FcstDataStore):
         if variables:
             self.select_variables(variables)
         
-        if self._mapping:
+        if self._mapping and self._stacked:
             self._data = add_xy(self._data,self._mapping)
+        elif self._mapping and not self._stacked:
+            LOG.warning("Ignoring `mapping` for already structured input data")
 
         LOG.info("Finished initializing AnemoiInference datastore")
 
@@ -105,12 +120,12 @@ class AnemoiInference(GridDataStore,FcstDataStore):
         """
         if isinstance(self._filename_or_obj, list):
             if len(self._filename_or_obj) > 0 and isinstance(self._filename_or_obj[0], xr.Dataset):
-                datasets = [_preprocess(ds) for ds in self._filename_or_obj]
+                datasets = [_preprocess(ds, self._input_format) for ds in self._filename_or_obj]
                 ds = xr.concat(datasets, dim="reference_time")
             else:
                 ds = xr.open_mfdataset(
                     self._filename_or_obj,
-                    preprocess=_preprocess,
+                    preprocess=lambda x: _preprocess(x, self._input_format),
                     chunks={
                         "reference_time" : 1,
                         "time": -1,
@@ -119,11 +134,11 @@ class AnemoiInference(GridDataStore,FcstDataStore):
                     **self._mf_kwargs,
                 )
         elif isinstance(self._filename_or_obj, xr.Dataset):
-            ds = _preprocess(self._filename_or_obj)
+            ds = _preprocess(self._filename_or_obj, self._input_format)
         else:
             ds = xr.open_mfdataset(
                 [self._filename_or_obj],
-                preprocess=_preprocess,
+                preprocess=lambda x: _preprocess(x, self._input_format),
                 chunks={
                     "reference_time" : 1,
                     "time": -1,
@@ -131,19 +146,29 @@ class AnemoiInference(GridDataStore,FcstDataStore):
                 },
                 **self._mf_kwargs,
             )
-        ds_coords = ds.assign_coords(
-            {
-                "lead_time": ("lead_time", self._lead_times),
-                "grid_index": ("grid_index", np.arange(ds.sizes["grid_index"])),
-                "valid_time": (
-                    ["reference_time", "lead_time"],
-                    ds["reference_time"].data[:,np.newaxis] + \
-                        self._lead_times[np.newaxis,:]
-                ),
-                "longitude" : ("grid_index", self._longitudes),
-                "latitude": ("grid_index", self._latitudes),
-            }
-        )
+        if self._input_format == "structured":
+            ds_coords = ds.assign_coords(
+                {
+                    "lead_time": ("lead_time", self._lead_times),
+                    "valid_time": (
+                        ["reference_time", "lead_time"],
+                        ds["reference_time"].data[:,np.newaxis] + self._lead_times[np.newaxis,:]
+                    ),
+                }
+            )
+        else:
+            ds_coords = ds.assign_coords(
+                {
+                    "lead_time": ("lead_time", self._lead_times),
+                    "grid_index": ("grid_index", np.arange(ds.sizes["grid_index"])),
+                    "valid_time": (
+                        ["reference_time", "lead_time"],
+                        ds["reference_time"].data[:,np.newaxis] + self._lead_times[np.newaxis,:]
+                    ),
+                    "longitude" : ("grid_index", self._longitudes),
+                    "latitude": ("grid_index", self._latitudes),
+                }
+            )
         ds_coords.attrs["is_observation"] = False
         return ds_coords
 
@@ -164,7 +189,7 @@ class AnemoiInference(GridDataStore,FcstDataStore):
         """
         LOG.debug(f"Start unstacking, stacked state is currently: {self._stacked}")
         if not self._stacked:
-            pass
+            return
 
         if mapping != None:
             if self._mapping != None:
@@ -201,9 +226,16 @@ def _calc_lead_times(ds: xr.Dataset | xr.DataArray) -> NDArray[np.timedelta64]:
         Returns:
             numpy.ndarray: An array of lead times relative to the first time value.
         """
-        return (ds["time"]- ds["time"][0]).data
+        if "lead_time" in ds.coords:
+            return ds["lead_time"].data
 
-def _preprocess(ds: xr.Dataset | xr.DataArray) -> xr.Dataset:
+        if "time" not in ds.coords:
+            raise ValueError("Cannot infer lead_time: dataset must contain `time` or `lead_time` coordinate")
+
+        reference_time = _infer_reference_time(ds)
+        return (ds["time"].data - reference_time).astype("timedelta64[ns]")
+
+def _preprocess(ds: xr.Dataset | xr.DataArray, input_format: str) -> xr.Dataset:
     """
     Preprocess the dataset by dropping unnecessary variables and renaming dimensions.
     This function drops specified variables from the dataset and renames dimensions
@@ -217,15 +249,33 @@ def _preprocess(ds: xr.Dataset | xr.DataArray) -> xr.Dataset:
         xarray.Dataset: The preprocessed dataset with dropped variables and renamed dimensions.
     """
 
-    reference_time = ds["time"].data[0]
-    
+    if input_format == "structured":
+        return _preprocess_structured(ds)
+    return _preprocess_unstructured(ds)
+
+
+def _infer_input_format(ds: xr.Dataset | xr.DataArray) -> str:
+    if "values" in ds.dims:
+        return "unstructured"
+    if "x" in ds.dims and "y" in ds.dims:
+        return "structured"
+    raise ValueError("Unsupported Anemoi inference format, expected dims containing `values` or (`y`, `x`)")
+
+
+def _infer_reference_time(ds: xr.Dataset | xr.DataArray) -> np.datetime64:
+    if "forecast_reference_time" in ds:
+        return np.asarray(ds["forecast_reference_time"].data).reshape(-1)[0].astype("datetime64[ns]")
+    if "reference_time" in ds.coords:
+        return np.asarray(ds["reference_time"].data).reshape(-1)[0].astype("datetime64[ns]")
+    return np.asarray(ds["time"].data).reshape(-1)[0].astype("datetime64[ns]")
+
+
+def _preprocess_unstructured(ds: xr.Dataset | xr.DataArray) -> xr.Dataset:
+    reference_time = _infer_reference_time(ds)
+
     ds_pruned = ds.drop_vars(DROP_VARS, errors="ignore")
-    ds_reftime = ds_pruned.expand_dims(
-        reference_time=[reference_time]
-    )
-    ds_reftime[
-        "reference_time"
-    ].attrs["standard_name"] = "forecast_reference_time"
+    ds_reftime = ds_pruned.expand_dims(reference_time=[reference_time])
+    ds_reftime["reference_time"].attrs["standard_name"] = "forecast_reference_time"
 
     ds_renamed = ds_reftime.rename_dims(
         {
@@ -234,3 +284,26 @@ def _preprocess(ds: xr.Dataset | xr.DataArray) -> xr.Dataset:
         }
     )
     return ds_renamed
+
+
+def _preprocess_structured(ds: xr.Dataset | xr.DataArray) -> xr.Dataset:
+    reference_time = _infer_reference_time(ds)
+
+    ds_work = ds.drop_vars(["forecast_reference_time"], errors="ignore")
+    if "latitude" in ds_work.data_vars:
+        ds_work = ds_work.assign_coords(latitude=ds_work["latitude"])
+    if "longitude" in ds_work.data_vars:
+        ds_work = ds_work.assign_coords(longitude=ds_work["longitude"])
+
+    if "time" in ds_work.dims:
+        lead_times = (ds_work["time"].data - reference_time).astype("timedelta64[ns]")
+        ds_work = ds_work.rename({"time": "lead_time"})
+        ds_work = ds_work.assign_coords(lead_time=("lead_time", lead_times))
+    elif "lead_time" not in ds_work.coords:
+        raise ValueError("Structured input must contain a `time` dimension or `lead_time` coordinate")
+
+    ds_work = ds_work.expand_dims(reference_time=[reference_time])
+    ds_work["reference_time"].attrs["standard_name"] = "forecast_reference_time"
+
+    extra_dims = [dim for dim in ds_work.dims if dim not in ["reference_time", "lead_time"]]
+    return ds_work.transpose("reference_time", "lead_time", *extra_dims)
